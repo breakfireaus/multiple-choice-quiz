53eb17ffd0c7888f5ed5e9b8c9a44ed72adbc306
breakfireaus <myoungerc@hotmail.com>
2022-09-03 13:14:18 +1000

    updated questions.js  highscores.html created scores.js

---
+++ assets/questions.js
@@ -0,0 +1,115 @@
+//list of all questions, choices, and answer.
+
+
+
+var questions = [
+    {
+        title: 'Which one of the following JavaScript is a looping structure?'
+        choices:[
+            'For', 
+            'While', 
+            'do-while loops', 
+            'All of the above'], 
+        answer: 'All of the above'
+    },
+
+    {
+        title: 'What are the basic groups of data types?'
+        choices: [
+            'Primitive and attribute',
+            'Reference types and Primitive'
+            'Attribute and reference types'
+            'None of the above'
+
+        ],
+        answer: 'Reference types and Primitive'
+            },
+
+            {
+        title: ' Which of the following are not commonly used data types?'
+        choices: [
+            'strings',
+            'alerts',
+            'booleans',
+            'numbers',
+        ]
+        answer: 'alerts'
+                    },
+
+                    {
+        title: 'Which of the following operators can be used in JavaScript?'
+        choices: [
+            '"And" Operator &&',
+            '"or" Operator ||',
+            '"Not" Operator !',
+            '"All of the above',
+        ],
+        answer: 'All of the above'
+                            },
+
+        {
+        title: 'What of the following are not three different types of errors in Javascript?'
+        choices:[
+            'animation time errors',
+            'load time errors',
+            'Run time errors',
+            'logical errors',
+        ]
+
+        answer: 'Animation time errors'
+        },
+
+        {
+        title: 'Fill in the blank: Arrays in JavaScript can be used to store_______. '
+        choices:[
+            'numbers and strings',
+            'other arrays',
+            'booleans',
+            'all of the above'
+        ]
+
+        answer: 'all of the above'
+        },
+
+        {
+            title: 'Fill in the blank: The condition in a if/else statement is enclosed within_____. '
+            choices:[
+                'qoutes',
+                'curly brackets',
+                'commas',
+                'parentheses'
+            ]
+            answer: 'parentheses'
+            },
+
+        {
+            title: 'Fill in the blank: String values must be enclosed within ______ when being assigned to variables. '
+            choices:[
+                'qoutes',
+                'curly brackets',
+                'commas',
+                'parentheses'
+            ]
+            answer: 'qoutes'
+            },
+        {
+            title: 'A useful tool that is good to use for debugging and development for content printing is?'
+            choices:[
+                'JavaScript',
+                'Terminal/bash',
+                'console.log',
+                'for loops'
+                ]
+                answer: 'console.log'
+                },
+        {
+            title: 'What is the type of Pop up boxes available in JavaScript?'
+            choices:[
+                'Alert',
+                'Confirm',
+                'Prompt',
+                'All of the above'
+                ]
+                answer: 'console.log'
+                },
+]
