47c23231e09676c0d0ceab7499a9eca46b1367ed
breakfireaus <myoungerc@hotmail.com>
2022-09-03 13:50:44 +1000

    fixed errors questions.js

--- assets/questions.js
+++ assets/questions.js
@@ -4,40 +4,40 @@
 
 var questions = [
     {
-        title: 'Which one of the following JavaScript is a looping structure?'
+        title: 'Which one of the following JavaScript is a looping structure?',
         choices:[
             'For', 
             'While', 
             'do-while loops', 
             'All of the above'], 
-        answer: 'All of the above'
+        answer: 'All of the above',
     },
 
     {
-        title: 'What are the basic groups of data types?'
+        title: 'What are the basic groups of data types?',
         choices: [
             'Primitive and attribute',
-            'Reference types and Primitive'
-            'Attribute and reference types'
-            'None of the above'
+            'Reference types and Primitive',
+            'Attribute and reference types',
+            'None of the above',
 
         ],
-        answer: 'Reference types and Primitive'
+        answer: 'Reference types and Primitive',
             },
 
             {
-        title: ' Which of the following are not commonly used data types?'
+        title: ' Which of the following are not commonly used data types?',
         choices: [
             'strings',
             'alerts',
             'booleans',
             'numbers',
         ]
-        answer: 'alerts'
+        ,answer: 'alerts',
                     },
 
                     {
-        title: 'Which of the following operators can be used in JavaScript?'
+        title: 'Which of the following operators can be used in JavaScript?',
         choices: [
             '"And" Operator &&',
             '"or" Operator ||',
@@ -48,7 +48,7 @@ var questions = [
                             },
 
         {
-        title: 'What of the following are not three different types of errors in Javascript?'
+        title: 'What of the following are not three different types of errors in Javascript?',
         choices:[
             'animation time errors',
             'load time errors',
@@ -56,11 +56,11 @@ var questions = [
             'logical errors',
         ]
 
-        answer: 'Animation time errors'
+        ,answer: 'Animation time errors',
         },
 
         {
-        title: 'Fill in the blank: Arrays in JavaScript can be used to store_______. '
+        title: 'Fill in the blank: Arrays in JavaScript can be used to store_______. ',
         choices:[
             'numbers and strings',
             'other arrays',
@@ -68,48 +68,48 @@ var questions = [
             'all of the above'
         ]
 
-        answer: 'all of the above'
+        ,answer: 'all of the above'
         },
 
         {
-            title: 'Fill in the blank: The condition in a if/else statement is enclosed within_____. '
-            choices:[
+        title: 'Fill in the blank: The condition in a if/else statement is enclosed within_____. ',
+        choices:[
                 'qoutes',
                 'curly brackets',
                 'commas',
                 'parentheses'
             ]
-            answer: 'parentheses'
+        ,answer: 'parentheses',
             },
 
         {
-            title: 'Fill in the blank: String values must be enclosed within ______ when being assigned to variables. '
+            title: 'Fill in the blank: String values must be enclosed within ______ when being assigned to variables. ',
             choices:[
                 'qoutes',
                 'curly brackets',
                 'commas',
                 'parentheses'
             ]
-            answer: 'qoutes'
+            ,answer: 'qoutes'
             },
         {
-            title: 'A useful tool that is good to use for debugging and development for content printing is?'
+            title: 'A useful tool that is good to use for debugging and development for content printing is?',
             choices:[
                 'JavaScript',
                 'Terminal/bash',
                 'console.log',
                 'for loops'
                 ]
-                answer: 'console.log'
+            ,answer: 'console.log',
                 },
         {
-            title: 'What is the type of Pop up boxes available in JavaScript?'
+            title: 'What is the type of Pop up boxes available in JavaScript?',
             choices:[
                 'Alert',
                 'Confirm',
                 'Prompt',
-                'All of the above'
+                'All of the above',
                 ]
-                answer: 'console.log'
+            ,answer: 'console.log',
                 },
 ]
